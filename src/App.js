c8667e9adf9de310d5d55fcf5759a505d2973ef8
akif <akifmansoori8287@gmail.com>
2024-09-21 22:45:52 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [userName, setUserName] = useState('')
+  const [userData, setUserData] = useState({})
+  useEffect(() => {
+
+  }, [])
+
+  const handleTextChange = (e) => {
+    const userName = e.target.value
+    setUserName(userName)
+  }
+
+  const getUserProfile = () => {
+    fetch(`https://api.github.com/users/${userName}`).then(
+      res => res.json()).then(result => setUserData({ ...result }))
+  }
+
+  const handleSearch = () => {
+    getUserProfile()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App container">
+        <h1>Github Profile Finder</h1>
+        <div className='search-bar-container'>
+          <input autoFocus required placeholder='Type username' className='search-bar' type="text" onChange={handleTextChange} />
+          <button type='submit' className='default-btn' onClick={getUserProfile}>
+            {" "}
+            Search{" "}
+          </button>
+        </div>
+        {userData.name ?
+          <>
+            <div className='profile-container'>
+              <img src={userData.avatar_url} alt="avatar_url" />
+              <section>
+                <p><span>Name: </span>{userData.name}</p>
+                <p><span>Address: </span>{userData.location}</p>
+                <p><span>Bio: </span>{userData.bio}</p>
+                <p><span>Profile url: </span>
+                  <a href={userData.html_url} target='_blank'>{userData.html_url}</a>
+                </p>
+                <p><span>Blog: </span>
+                  <a href={userData.blog} target='_blank'>{userData.blog}</a>
+                </p>
+              </section>
+            </div>
+          </>
+          : <p>user not found</p>
+        }
+      </div>
+    </>
   );
 }
 
